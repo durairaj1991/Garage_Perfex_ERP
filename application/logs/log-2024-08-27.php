5948b8b73a79301f45267f1bc81be4e2c72b5391
SAM <sam@SAM.DHANWANTRI>
2024-08-28 15:22:50 +0530

    Initial commit

---
+++ application/logs/log-2024-08-27.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-08-27 14:49:07 --> Severity: error --> Exception: Unknown database 'prefex' C:\xampp\htdocs\erp\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-08-27 16:09:29 --> Could not find the language line "create_new_job_card"
+ERROR - 2024-08-27 16:10:08 --> Could not find the language line "create_new_job_card"
+ERROR - 2024-08-27 16:10:52 --> Severity: Warning --> Undefined variable $estimate C:\xampp\htdocs\erp\application\views\admin\estimates\estimate.php 17
+ERROR - 2024-08-27 16:10:52 --> Could not find the language line "create_new_job_card"
+ERROR - 2024-08-27 16:11:10 --> Could not find the language line "create_new_job_card"
+ERROR - 2024-08-27 19:20:10 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 19:24:51 --> Could not find the language line "Journal Entry"
+ERROR - 2024-08-27 19:24:51 --> Could not find the language line "add"
+ERROR - 2024-08-27 19:24:51 --> Could not find the language line "reference"
+ERROR - 2024-08-27 19:32:06 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 19:33:04 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 19:48:48 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 19:49:42 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 19:52:28 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 19:53:40 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 19:55:07 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 19:00:20 --> 404 Page Not Found: /index
+ERROR - 2024-08-27 20:01:18 --> Could not find the language line "Registers"
+ERROR - 2024-08-27 20:01:18 --> Could not find the language line "acc_user_register"
+ERROR - 2024-08-27 20:01:18 --> Could not find the language line "acc_submit"
+ERROR - 2024-08-27 20:43:37 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 20:43:53 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 20:43:58 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 20:44:58 --> Could not find the language line "num_word_QAR"
+ERROR - 2024-08-27 20:51:50 --> Severity: 8192 --> Creation of dynamic property AccountingLic::$check_interval_file is deprecated /var/www/html/automotive/modules/accounting/libraries/gtsslib.php 59
+ERROR - 2024-08-27 20:51:51 --> Severity: 8192 --> Creation of dynamic property AccountingLic::$check_interval_file is deprecated /var/www/html/automotive/modules/accounting/libraries/gtsslib.php 59
+ERROR - 2024-08-27 17:51:51 --> 404 Page Not Found: /index
+ERROR - 2024-08-27 20:51:53 --> Severity: 8192 --> Creation of dynamic property AccountingLic::$check_interval_file is deprecated /var/www/html/automotive/modules/accounting/libraries/gtsslib.php 59
+ERROR - 2024-08-27 20:51:53 --> Severity: Warning --> Undefined array key "id" /var/www/html/automotive/modules/accounting/views/banking/banking_feeds.php 179
+ERROR - 2024-08-27 20:52:20 --> Severity: 8192 --> Creation of dynamic property AccountingLic::$check_interval_file is deprecated /var/www/html/automotive/modules/accounting/libraries/gtsslib.php 59
+ERROR - 2024-08-27 20:52:20 --> Severity: Warning --> Undefined array key "id" /var/www/html/automotive/modules/accounting/views/banking/banking_feeds.php 179
